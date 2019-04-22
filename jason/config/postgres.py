1ad5958984cedacbc2e8473aa33cbf39c248458d
Adam Romano <manoadamro.imp@gmail.com>
2019-04-22 17:46:49 +0100

    added some docstrings to the postgres config mixin

--- jason/config/postgres.py
+++ jason/config/postgres.py
@@ -14,10 +14,20 @@ class PostgresConfigMixin:
 
 
 def scoped_session(factory):
+    """
+    creates a thread safe session factory
+
+    """
     return sqlalchemy.orm.scoped_session(factory)
 
 
 def postgres_engine(config: PostgresConfigMixin, testing=False):
+    """
+    builds a connection uri based on config options and returns an engine.
+    
+    NOTE: if testing is true, the uri will be to an in-memory sqlite database
+
+    """
     if not testing:
         if config.DB_USER:
             credentials = f"{config.DB_USER}:{config.DB_PASS}@"
