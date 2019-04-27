b88f2e9f92d8d5bda90d355958facab8aa9cccf0
Adam Romano <manoadamro.imp@gmail.com>
2019-04-27 23:04:05 +0100

    fixed dependency list in setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ setuptools.setup(
     name='jason',
     version='0.0.15',
     install_requires=[
+        'fire==0.1.3',
         "Flask==1.0.2",
         "jsonpointer==2.0",
+        'pika==0.13.1',
         "pycryptodome==3.8.1",
         "PyJWT==1.7.1",
         'redis==3.2.1',
@@ -19,9 +21,7 @@ setuptools.setup(
         'dev': [
             'black==18.9b0',
             'coverage==4.5.1',
-            'fire==0.1.3',
             'isort==4.3.4',
-            'pika==0.13.1',
             'pytest==4.4.1',
         ],
     },
