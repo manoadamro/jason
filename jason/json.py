90af52a1fc6db070d0ec7766f48a9bcbb9a515ae
Adam Romano <manoadamro.imp@gmail.com>
2019-04-28 09:28:29 +0100

    added custom json encoder to handle datetimes and dates

---
+++ jason/json.py
@@ -0,0 +1,20 @@
+import json
+import datetime
+
+dump = json.dump
+dumps = json.dumps
+
+load = json.load
+loads = json.loads
+
+
+class JsonEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, (datetime.datetime, datetime.date)):
+            return obj.isoformat()
+        super(JsonEncoder, self).default(obj)
+
+
+class JsonDecoder(json.JSONDecoder):
+    ...
