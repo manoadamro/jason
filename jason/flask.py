1adc7e255350e2e6de12e86579f9b394d6b561cb
Adam Romano <manoadamro.imp@gmail.com>
2019-04-28 20:52:48 +0100

    added basic create app method

--- jason/flask.py
+++ jason/flask.py
@@ -1,9 +1,17 @@
+import flask
 import waitress
 
 from jason.config import Config, props
+from .database import Database
+from .cache import RedisCache
+from .utils import is_instance_or_type,is_type
 
 Config = Config
 props = props
+
+
+db = Database()
+cache = RedisCache()
 
 
 class FlaskConfigMixin:
@@ -18,5 +26,18 @@ def serve(app, config, testing=False):
         waitress.serve(app, host=config.SERVE_HOST, port=config.SERVE_PORT)
 
 
-def create_app():
-    ...
+def create_app(config, testing=False, use_db=False, use_cache=False):
+    app = flask.Flask(__name__)
+    testing.testing = testing
+    if not is_instance_or_type(config, Config):
+        raise TypeError(f"config object must be an instance or subclass of {Config.__name__}")
+    if is_type(config, Config):
+        config = config.load()
+    app.config.update(config.__dict__)
+    if use_db:
+        db.init(config=config, testing=testing)
+        app.config["DB"] = db
+    if use_cache:
+        cache.init(config=config, testing=testing)
+        app.config["CACHE"] = cache
+    return app
