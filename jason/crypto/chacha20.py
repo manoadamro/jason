aa77e0c1da763440aba01c34a4ed6f0721936049
Adam Romano <manoadamro.imp@gmail.com>
2019-04-30 22:10:58 +0100

    broke up crypto module

--- jason/crypto/chacha20.py
+++ jason/crypto/chacha20.py
@@ -5,10 +5,13 @@ from Crypto.Cipher import ChaCha20 as _ChaCha20
 
 
 class ChaCha20:
-    escape = ("-", "%-")
+    encoding = "utf8"
+    join_char = "-"
+    join_string = join_char * 2
+    escape = (join_char, f"%{join_char}")
 
     def __init__(self, key):
-        self.key = key.rjust(32).encode("utf8")
+        self.key = key.rjust(32).encode(self.encoding)
 
     def _escape(self, string):
         return re.sub(self.escape[0], self.escape[1], string)
@@ -18,15 +21,15 @@ class ChaCha20:
 
     def encrypt(self, plain_text: str) -> str:
         cipher = _ChaCha20.new(key=self.key)
-        cipher_bytes = cipher.encrypt(plain_text.encode("utf8"))
-        nonce = base64.b64encode(cipher.nonce).decode("utf-8")
-        cipher_text = base64.b64encode(cipher_bytes).decode("utf-8")
-        return f"{self._escape(cipher_text)}---{self._escape(nonce)}"
+        cipher_bytes = cipher.encrypt(plain_text.encode(self.encoding))
+        nonce = base64.b64encode(cipher.nonce).decode(self.encoding)
+        cipher_text = base64.b64encode(cipher_bytes).decode(self.encoding)
+        return f"{self._escape(cipher_text)}{self.join_string}{self._escape(nonce)}"
 
     def decrypt(self, encrypted: str) -> str:
-        cipher_text, nonce = encrypted.split("---")
+        cipher_text, nonce = encrypted.split(self.join_string)
         cipher_text = base64.b64decode(self._un_escape(cipher_text))
         nonce = base64.b64decode(self._un_escape(nonce))
         cipher = _ChaCha20.new(key=self.key, nonce=nonce)
         plain_text = cipher.decrypt(cipher_text)
-        return plain_text.decode("utf8")
+        return plain_text.decode(self.encoding)
