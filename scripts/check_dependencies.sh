148e1a4302e5265e03d744ca6528be5c70f57173
Adam Romano <manoadamro.imp@gmail.com>
2019-04-30 15:44:58 +0100

    circle to create egg info

--- scripts/check_dependencies.sh
+++ scripts/check_dependencies.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+set -e
 
-sh ./scripts/list_dependencies.sh  | cut -d = -f 1 | xargs -n 1 pip3 search | grep -B2 'LATEST:'
+sh ./scripts/list_dependencies.sh  | cut -d = -f 1 | xargs -n 1 pip3 search | grep -B2 'LATEST:';
