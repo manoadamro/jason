773c165dda451cf58c68164c24184c5c3de92c88
Adam Romano <manoadamro.imp@gmail.com>
2019-05-03 20:30:13 +0100

    small fixes to props.Compound

--- jason/props/types/compound.py
+++ jason/props/types/compound.py
@@ -10,7 +10,9 @@ from .property import Property
 class Compound(Inline):
     def __init__(
         self,
-        *objects: Union[Model, Nested, Dict[str, Property], Type[Model]],
+        *objects: Union[
+            Model, Nested, Dict[str, Union[Property, Type[Property]]], Type[Model]
+        ],
         **kwargs: Any,
     ):
         schema = {}
@@ -32,13 +34,14 @@ class Compound(Inline):
                 if not utils.is_instance_or_type(value, (Model, base.SchemaAttribute)):
                     if not utils.is_type(value):
                         value = type(value)
-                    raise error.PropertyValidationError(
-                        f"can only combine objects of type: "
+                    raise ValueError(
+                        f"can only combine objects of types: "
                         f"'Model', 'SchemaAttribute'. Not '{value}'"
                     )
-                if key in schema:
-                    raise error.PropertyValidationError(
-                        f"A property with name '{key}' already exists in combined object."
+                if key in schema and schema[key] != value:
+                    raise ValueError(
+                        f"A property with name '{key}' already exists in combined object but is not identical. "
+                        f"{schema[key].__dict__} != {value.__dict__}"
                     )
                 schema[key] = value
         super(Compound, self).__init__(props=schema, **kwargs)
