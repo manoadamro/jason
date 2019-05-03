584cd30b455e0262a2c2d77e44b7282611e00442
Adam Romano <manoadamro.imp@gmail.com>
2019-05-03 21:06:37 +0100

    added deep compare method to utils

--- jason/props/utils.py
+++ jason/props/utils.py
@@ -18,3 +18,20 @@ def is_type(value, typ=None):
 
 def is_instance_or_type(value, typ):
     return is_type(value, typ=typ) or isinstance(value, typ)
+
+
+def deep_compare(base, compare):
+    if not hasattr(base, "__dict__") or not hasattr(compare, "__dict__"):
+        print(f"straight comparison {type(base)} -> {base} | {compare}")
+        return base == compare
+    if len(base.__dict__) != len(compare.__dict__):
+        print(f"length of {type(base)} -> {base} | {compare}")
+        return False
+    for key, value in base.__dict__.items():
+        if key not in compare.__dict__:
+            print(f"value of {key} -> {value} | {compare.__dict__[key]}")
+            return False
+        if not deep_compare(value, compare.__dict__[key]):
+            print(f"deep compare {key} -> {value} | {compare.__dict__[key]}")
+            return False
+    return True
