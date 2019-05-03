9b63912c430f3198d83f8d9725942583397c9176
Adam Romano <manoadamro.imp@gmail.com>
2019-05-03 14:21:55 +0100

    added tests for props.model

---
+++ tests/unit/props/types/test_model.py
@@ -0,0 +1,19 @@
+import pytest
+
+from jason import props
+
+
+def test_handles_prop_types():
+    class MyModel(props.Model):
+        x = props.Int
+        _y = props.Int
+
+    assert isinstance(MyModel.__props__["x"], props.Int)
+
+
+def test_ignored_underscore_prefixed():
+    class MyModel(props.Model):
+        x = props.Int()
+        _y = props.Int()
+
+    assert MyModel.__props__ == {"x": MyModel.x}
