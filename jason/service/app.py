6b7332ffecedacc17038260195b074b5d3798d72
Adam Romano <manoadamro.imp@gmail.com>
2019-07-10 18:21:24 +0100

    added custom json encoder

--- jason/service/app.py
+++ jason/service/app.py
@@ -2,13 +2,18 @@ from typing import Any
 
 import flask
 
+from .encoder import JSONEncoder
+
 
 class App(flask.Flask):
-    def __init__(self, name: str, config: Any, testing: bool = False, **kwargs: Any):
+    json_encoder = JSONEncoder
+
+    def __init__(self, name: str, config: Any, testing: bool = False, json_encoder=None, **kwargs: Any):
         super(App, self).__init__(name, **kwargs)
         config.update(self.config)
         self.config = config
         self.testing = testing
+        self.json_encoder = json_encoder or self.json_encoder
 
     def assert_mixin(self, mixin, item, condition=""):
         if not isinstance(self.config, mixin):
